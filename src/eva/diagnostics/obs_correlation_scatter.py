2b57898c41b0fef3aa95abf44e4fac6816676151
Cory Martin <cory.r.martin@noaa.gov>
2022-02-18 15:32:09 -0500

    Changes to ObsCorrelationScatter to use new plotting methods (#7)
    * Create figure.py
    
    * Add in the rest of declarative plotting
    
    * Fix missing EOL
    
    * pycodestyle
    
    * more pycodestyle
    
    * Update obs_correlation_scatter.py
    
    * Remove mstats
    
    * Add missing save_figure method
    
    * Finish thought
    
    * Changes to plot
    
    * Small fixes
    
    * make dots blue
    
    * pycodestyle
    
    * Remove old plot routine
    
    * Move diagnostics into new, separate directory (#8)
    
    * Start to move file
    
    * add init file; modify base.py to import from eva.diagnostics
    
    * pycodestyle
    
    Co-authored-by: Dan Holdaway <27729500+danholdaway@users.noreply.github.com>

--- src/eva/diagnostics/obs_correlation_scatter.py
+++ src/eva/diagnostics/obs_correlation_scatter.py
@@ -1,3 +1,4 @@
+# This work developed by NOAA/NWS/EMC under the Apache 2.0 license.
 # (C) Copyright 2021-2022 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration. All Rights Reserved.
 #
@@ -11,7 +12,8 @@
 from eva.base import Base
 from eva.utilities import ioda_definitions
 from eva.utilities import ioda_netcdf_api
-from eva.plot_tools.scatter_correlation import scatter_correlation_plot
+from eva.plot_tools.figure import CreatePlot, CreateFigure
+from eva.plot_tools.plots import Scatter, LinePlot
 
 import netCDF4
 import numpy as np
@@ -179,9 +181,30 @@ class ObsCorrelationScatter(Base):
                             plot_title = platform_long_name + ' | ' + variable_name_no_
 
                             # Create the plot
-                            scatter_correlation_plot(data_ref, data_exp, ref_metric_long_name,
-                                                     exp_metric_long_name, plot_title, output_file,
-                                                     marker_size=marker_size)
+                            # set up the scatter layer
+                            scatter = Scatter(data_ref, data_exp)
+                            scatter.markersize = marker_size
+                            scatter.color = 'blue'
+                            data_min = min(min(data_ref), min(data_exp))
+                            data_max = max(max(data_ref), max(data_exp))
+                            data_diff = data_max - data_min
+                            plotmin = data_min - (0.1 * data_diff)
+                            plotmax = data_max + (0.1 * data_diff)
+                            # add a 1:1 line layer
+                            line = LinePlot([plotmin, plotmax], [plotmin, plotmax])
+                            line.color = 'black'
+                            # set up the plot
+                            plot = CreatePlot(plot_layers=[line, scatter])
+                            plot.add_title(plot_title)
+                            plot.set_xlim([plotmin, plotmax])
+                            plot.set_ylim([plotmin, plotmax])
+                            plot.add_xlabel(ref_metric_long_name)
+                            plot.add_ylabel(exp_metric_long_name)
+                            # create the figure
+                            fig = CreateFigure(figsize=(8, 8))
+                            fig.plot_list = [plot]
+                            fig.create_figure()
+                            fig.save_figure(output_file)
 
             # Close files
             fh_exp.close()
