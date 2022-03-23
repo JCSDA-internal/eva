bf5158e55a4d78766133ecb20fb71a91486598a3
danholdaway <danholdaway@users.noreply.github.com>
2022-03-23 17:18:34 -0400

    variable and metadata readable into xarray structures

---
+++ src/eva/data/ioda_obs_space.py
@@ -0,0 +1,97 @@
+# (C) Copyright 2021-2022 NOAA/NWS/EMC
+#
+# (C) Copyright 2021-2022 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+# --------------------------------------------------------------------------------------------------
+
+import xarray as xr
+
+from eva.eva_base import EvaBase
+from eva.utilities.config import get
+
+# --------------------------------------------------------------------------------------------------
+
+
+class IodaObsSpace(EvaBase):
+
+    def execute(self, data_collections):
+
+        # Local copies
+        logger = self.logger
+        config = self.config
+
+        # Get the list of datasets to create
+        datasets = config.get('datasets')
+
+        # Loop over the datasets
+        for dataset in datasets:
+
+            # Get the groups to be read
+            groups = get(dataset, logger, 'groups')
+
+            # Assert the correct use of variables and channels
+            if ('variables' in dataset and 'channels' in dataset):
+                self.logger.abort('Dataset configuraiton for \'' + dataset['name'] + '\'' +
+                                  'contains both channels or variables, should be one or ' +
+                                  'the other. If channels present, variable is ' +
+                                  'brightness_temperature')
+
+            # Get channels or variables
+            if ('variables' in dataset):
+                variables = dataset['variables']
+            elif ('channels' in dataset):
+                channels = dataset['channels']
+                variables = ['brightness_temperature']  # If channels present variable is bt
+            else:
+                self.logger.abort('Dataset configuraiton for \'' + dataset['name'] + '\'' +
+                                  'contains neither channels or variables.')
+
+            # Get metadata
+            metadata_variables = dataset.get('metadata', [])
+
+            # Add metadata group to list
+            if metadata_variables:
+                groups.append('MetaData')
+
+            # Filenames to be read into this collection
+            filenames = get(dataset, logger, 'filenames')
+
+            # Loop over groups
+            for group in groups:
+
+                # Set the variables
+                variables_need = variables
+                if group is 'MetaData':
+                    variables_need = metadata_variables
+
+                # Set the collection name
+                collection_name = dataset['name'] + '::' + group
+
+                # Loop over filenames
+                for filename in filenames:
+
+                    # Open file and add collection
+                    with xr.open_dataset(filename, group=group) as ds:
+
+                        # Drop data variables not in user requested variables
+                        vars_to_remove = list(set(list(ds.keys())) - set(variables_need))
+                        ds = ds.drop_vars(vars_to_remove)
+
+                        # Assert that the collection contains at least one variable. If not it is
+                        # likely some problem has occurred
+                        if not ds.keys():
+                            self.logger.abort('Collection \'' +  dataset['name'] + ', group ' +
+                                               group + '\' in file ' + filename +
+                                               ' does not have any variables.')
+
+                        # Add the dataset to the collections
+                        data_collections.create_or_add_to_collection(collection_name, ds, 'nlocs')
+
+
+            print(data_collections)
+
+            exit()
---
+++ src/eva/utilities/config.py
@@ -0,0 +1,76 @@
+# (C) Copyright 2021-2022 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+# --------------------------------------------------------------------------------------------------
+
+from eva.utilities.utils import load_yaml_file
+
+# --------------------------------------------------------------------------------------------------
+#  @package config
+#
+#  Class containing a config for tasks.
+#
+# --------------------------------------------------------------------------------------------------
+
+
+class Config(dict):
+
+    # ----------------------------------------------------------------------------------------------
+
+    def __init__(self, dict_or_yaml, logger):
+
+        # Copy of logger
+        self.logger = logger
+
+        # Program can recieve a dictionary or a yaml file
+        if isinstance(dict_or_yaml, dict):
+            self.config = dict_or_yaml
+        else:
+            self.config = load_yaml_file(dict_or_yaml, logger)
+
+        # Initialize the parent class with the config
+        super().__init__(self.config)
+
+    # ----------------------------------------------------------------------------------------------
+
+    def get(self, key, default=None):
+
+        if default is None:
+
+            if key in self.config:
+
+                return super().get(key)
+
+            else:
+
+                self.logger.abort("Configuraiton does not have the key")
+
+        else:
+
+            return super().get(key, default)
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+def get(dict, logger, key, default=None):
+
+    if default is None:
+
+        if key in dict:
+
+            return dict.get(key)
+
+        else:
+
+            logger.abort("Configuration does not have the key")
+
+    else:
+
+        return dict.get(key, default)
+
+
+# --------------------------------------------------------------------------------------------------
