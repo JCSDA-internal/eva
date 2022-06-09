a5a784953479132080fa3a2ea5b9e9d3dc08cd68
Dan Holdaway <27729500+danholdaway@users.noreply.github.com>
2022-06-09 15:17:00 -0400

    Bugfix/zero len data (#31)
    * Bugfix in _scatter to ensure regression is not computed for no data
    
    * bump version
    
    Co-authored-by: danholdaway <danholdaway@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='eva',
-    version='1.2.0',
+    version='1.2.1',
     author='Community owned code',
     description='Evaluation and Verification of an Analysis',
     url='https://github.com/danholdaway/eva',
