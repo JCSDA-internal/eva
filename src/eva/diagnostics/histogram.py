a9df1efc08aca0fc66060336c43b606bc0dac190
Cory Martin <cory.r.martin@noaa.gov>
2022-07-25 15:53:25 -0500

    end of day commit; start of histogram class

---
+++ src/eva/diagnostics/histogram.py
@@ -0,0 +1,28 @@
+from eva.eva_path import return_eva_path
+from eva.utilities.config import get
+from eva.utilities.utils import get_schema, update_object, slice_var_from_str
+import eva.plot_tools.plots
+import os
+import numpy as np
+
+
+class Histogram():
+
+    def __init__(self, config, logger, dataobj):
+
+        # Get the data to plot from the data_collection
+        # ---------------------------------------------
+        varstr = config['variable']
+        var_cgv = varstr.split('::')
+
+        if len(var_cgv) != 3:
+            logger.abort('In Histogram the variable \'var_cgv\' does not appear to '+
+                         'be in the required format of collection::group::variable.')
+
+
+        # Optionally get the channel to plot
+        channel = None
+        if 'channel' in config:
+            channel = config.get('channel')
+
+        data = dataobj.get_variable_data(var_cgv[0], var_cgv[1], var_cgv[2], channel)
