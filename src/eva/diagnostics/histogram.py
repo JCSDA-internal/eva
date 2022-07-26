c01881f9eeb7b2e21e16b43900bc86f244f0c809
Cory Martin <cory.r.martin@noaa.gov>
2022-07-26 07:46:41 -0500

    Finished with first pass at histogram.py, need schema and example

--- src/eva/diagnostics/histogram.py
+++ src/eva/diagnostics/histogram.py
@@ -12,7 +12,7 @@ class Histogram():
 
         # Get the data to plot from the data_collection
         # ---------------------------------------------
-        varstr = config['variable']
+        varstr = config['data']['variable']
         var_cgv = varstr.split('::')
 
         if len(var_cgv) != 3:
@@ -22,7 +22,31 @@ class Histogram():
 
         # Optionally get the channel to plot
         channel = None
-        if 'channel' in config:
+        if 'channel' in config['data']:
             channel = config.get('channel')
 
         data = dataobj.get_variable_data(var_cgv[0], var_cgv[1], var_cgv[2], channel)
+
+        # See if we need to slice data
+        data = slice_var_from_str(config['data'], data, logger)
+
+        # Histogram data should be flattened
+        data = data.flatten()
+
+        # Missing data should also be removed
+        mask = ~np.isnan(data)
+        data = data[mask]
+
+        # Create declarative plotting histogram object
+        # --------------------------------------------
+        self.plotobj = eva.plot_tools.plots.Histogram(data)
+
+        # Get defaults from schema
+        # ------------------------
+        layer_schema = config.get('schema', os.path.join(return_eva_path(), 'defaults',
+                                  'histogram.yaml'))
+        config = get_schema(layer_schema, config, logger)
+        delvars = ['type', 'schema', 'data']
+        for d in delvars:
+            config.pop(d, None)
+        self.plotobj = update_object(self.plotobj, config, logger)
