606f75552240e3d89cc1f6a05e2b245640bb155e
danholdaway <danholdaway@users.noreply.github.com>
2022-08-05 15:40:18 -0400

    action testing

--- src/eva/utilities/stats.py
+++ src/eva/utilities/stats.py
@@ -212,10 +212,11 @@ def get_linear_regression(x, y):
         slope of the line from the equation for the predicted
         y values.
     """
-    from sklearn import linear_model
+    import sklearn
+    #from sklearn import linear_model
     # from sklearn.linear_model import LinearRegression
     x = x.reshape((-1, 1))
-    model = linear_model.LinearRegression().fit(x, y)
+    model = sklearn.linear_model.LinearRegression().fit(x, y)
     r_sq = model.score(x, y)
     intercept = model.intercept_
     slope = model.coef_[0]
