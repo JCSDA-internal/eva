346502ed3b914cfac25a3fa82d97783434144f93
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-10-27 15:04:32 +0000

    new emcpy hash

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
         'scikit-learn>=1.0.2',
         'xarray>=0.11.3',
         'emcpy @ git+https://github.com/NOAA-EMC/' +
-        'emcpy@4fce929ea432e738aa7e3a2502bd0ead7d32ae0a#egg=emcpy',
+        'emcpy@9e55f5c64a03dbca56ff9425a480d6906e834e17#egg=emcpy',
     ],
     package_data={
         '': [
