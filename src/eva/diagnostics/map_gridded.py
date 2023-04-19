fca698bba16b36c106ceaf67498d7e764686fb69
Kevin Dougherty <kevin.dougherty@noaa.gov>
2023-04-19 17:30:25 +0000

    updated map_gridded default yaml and script

--- src/eva/diagnostics/map_gridded.py
+++ src/eva/diagnostics/map_gridded.py
@@ -1,6 +1,6 @@
 from eva.eva_path import return_eva_path
 from eva.utilities.utils import get_schema, update_object, slice_var_from_str
-import emcpy.plots.plots
+import emcpy.plots.map_plots
 import os
 
 
@@ -8,14 +8,18 @@ class MapGridded():
 
     def __init__(self, config, logger, dataobj):
         # prepare data based on config
-        lonvar = dataobj.get_variable_data(config['longitude']['variable'])
+        lonvar_cgv = config['longitude']['variable'].split('::')
+        lonvar = dataobj.get_variable_data(lonvar_cgv[0], lonvar_cgv[1], lonvar_cgv[2], None)
         lonvar = slice_var_from_str(config['longitude'], lonvar, logger)
-        latvar = dataobj.get_variable_data(config['latitude']['variable'])
+        latvar_cgv = config['latitude']['variable'].split('::')
+        latvar = dataobj.get_variable_data(latvar_cgv[0], latvar_cgv[1], latvar_cgv[2], None)
         latvar = slice_var_from_str(config['latitude'], latvar, logger)
-        datavar = dataobj.get_variable_data(config['data']['variable'])
+        datavar_cgv = config['data']['variable'].split('::')
+        datavar = dataobj.get_variable_data(datavar_cgv[0], datavar_cgv[1], datavar_cgv[2], None)
         datavar = slice_var_from_str(config['data'], datavar, logger)
+
         # create declarative plotting MapGridded object
-        self.plotobj = emcpy.plots.plots.MapGridded(lonvar, latvar, datavar)
+        self.plotobj = emcpy.plots.map_plots.MapGridded(latvar, lonvar, datavar)
         # get defaults from schema
         layer_schema = config.get("schema",
                                   os.path.join(return_eva_path(),
