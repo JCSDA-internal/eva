a4e35a1b5f47d3b807879fd194a938ea3123a4b6
Kevin Dougherty <kevin.dougherty@noaa.gov>
2023-05-04 16:55:02 +0000

    fix typo

--- src/eva/data/cubed_sphere_restart.py
+++ src/eva/data/cubed_sphere_restart.py
@@ -1,4 +1,4 @@
-# (C) Copyright 20212-2023 NOAA/NWS/EMC
+# (C) Copyright 2022-2023 NOAA/NWS/EMC
 #
 # (C) Copyright 2022-2023 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration. All Rights Reserved.
