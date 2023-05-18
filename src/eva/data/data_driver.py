a56a0cb68edbb60704551c39f346c6f09d0c9b8e
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2023-05-18 14:44:19 +0000

    start of this data driver change

---
+++ src/eva/data/data_driver.py
@@ -0,0 +1,49 @@
+# (C) Copyright 2021-2023 NOAA/NWS/EMC
+#
+# (C) Copyright 2021-2023 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration. All Rights Reserved.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+from eva.utilities.config import get
+from eva.eva_base import EvaBase
+
+import importlib
+import os
+
+
+# --------------------------------------------------------------------------------------------------
+
+
+class DataDriver(EvaBase):
+
+    def execute(self, data_collections, timing):
+
+        # Get list of dataset dictionaries
+        datasets = get(self.config, self.logger, 'datasets')
+
+        # Loop over datasets
+        for dataset in datasets:
+
+            # Get the dataset type
+            dataset_type = get(dataset, self.logger, 'dataset')
+
+            # Replace spaces with underscore
+            dataset_type = dataset_type.replace(' ', '_')
+
+            # Instantiate the tranform object
+            dataset_method = getattr(importlib.import_module('eva.data.'+dataset_type),
+                                       dataset_type)
+
+            # Call the dataset
+            timing.start(f'dataset: {dataset_type}')
+            dataset_method(dataset, data_collections)
+            timing.stop(f'dataset: {dataset_type}')
+
+# --------------------------------------------------------------------------------------------------
