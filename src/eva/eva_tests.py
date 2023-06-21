e9fd4a419c305913d6d6c3e68d170e1478ae8546
Akira Sewnath <asewnath@users.noreply.github.com>
2023-06-21 15:19:13 -0400

    Update eva_tests.py

--- src/eva/eva_tests.py
+++ src/eva/eva_tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-# (C) Copyright 2021-2022 NOAA/NWS/EMC
+# (C) Copyright 2021-2023 NOAA/NWS/EMC
 #
-# (C) Copyright 2021-2022 United States Government as represented by the Administrator of the
+# (C) Copyright 2021-2023 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration. All Rights Reserved.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
