689ca8fe061303fc2d6a7ded96497f02f7941b0c
Kevin Dougherty <kevin.dougherty@noaa.gov>
2023-08-18 14:46:33 +0000

    fix format

--- src/eva/plotting/emcpy/diagnostics/horizontal_line.py
+++ src/eva/plotting/emcpy/diagnostics/horizontal_line.py
@@ -8,29 +8,32 @@ import os
 
 
 class HorizontalLine():
-    """
-    Creates a horizontal line plot based on the provided configuration.
-
-    Args:
-        config (dict): A dictionary containing the configuration for the horizontal line plot.
-        logger (Logger): An instance of the logger for logging messages.
-
-    This class initializes and configures a horizontal line plot based on the provided
-    configuration. The horizontal line plot is created using a declarative plotting library from
-    EMCPy (https://github.com/NOAA-EMC/emcpy).
-
-    Example:
-        config = {
-            "y": 0.5,
-            "plot_property": "property_value",
-            "plot_option": "option_value",
-            "schema": "path_to_schema_file.yaml"
-        }
-        logger = Logger()
-        horizontal_line_plot = HorizontalLine(config, logger)
-    """
+    
+    """Base class for creating horizontal line plots."""
 
     def __init__(self, config, logger, dataobj):
+
+        """
+        Creates a horizontal line plot based on the provided configuration.
+
+        Args:
+            config (dict): A dictionary containing the configuration for the horizontal line plot.
+            logger (Logger): An instance of the logger for logging messages.
+
+        This class initializes and configures a horizontal line plot based on the provided
+        configuration. The horizontal line plot is created using a declarative plotting library from
+        EMCPy (https://github.com/NOAA-EMC/emcpy).
+
+        Example:
+            config = {
+                "y": 0.5,
+                "plot_property": "property_value",
+                "plot_option": "option_value",
+                "schema": "path_to_schema_file.yaml"
+            }
+            logger = Logger()
+            horizontal_line_plot = HorizontalLine(config, logger)
+        """
 
         # Get the y value to plot
         # -----------------------
