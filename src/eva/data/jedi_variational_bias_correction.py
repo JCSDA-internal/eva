e1d11b21341d29ba4a40de7f9654f634b76905a4
danholdaway <danholdaway@users.noreply.github.com>
2024-05-14 14:58:06 -0400

    Update to new format for varBC files

--- src/eva/data/jedi_variational_bias_correction.py
+++ src/eva/data/jedi_variational_bias_correction.py
@@ -74,54 +74,44 @@ class JediVariationalBiasCorrection(EvaDatasetBase):
         bias_file = get(dataset_config, self.logger, 'bias_file')
         lapse_file = get(dataset_config, self.logger, 'lapse_file')
 
-        # Read the contents of the bias file (netCDF) into an xarray dataset
-        bias_dataset = xr.open_dataset(bias_file)
-
-        # Assign proper coordinates to the dataset and remove nchannels and npredictors from
-        # variables
-        bias_dataset = bias_dataset.assign_coords(
-            {'nchannels': range(len(bias_dataset.nchannels)),
-             'npredictors': range(len(bias_dataset.npredictors))
-             }
-             )
-
-        # Get all the predictor names
-        predictor_names = bias_dataset['predictors'].values
-
-        # Initialize a dictionary to store the new data variables
-        new_data_vars = {}
-
-        # To eva-rise the dateset we dont want predictor as a dimension, otherwise all the plotting
-        # code would have to be made to understand that as a dimension and make that part less
-        # generic. Instead we want more variables with predictor as the variable name and the type
-        # as the group.
-        vars_to_flatter = [
-            'bias_coeff_errors',
-            'bias_coefficients',
+        groups = [
+            'BiasCoefficients',
+            'BiasCoefficientErrors',
         ]
 
-        # Flatten into new variables with only channel dimension
-        for var in vars_to_flatter:
-            for i, predictor in enumerate(predictor_names):
-                new_data_vars[f'{var}::{predictor}'] = \
-                    (['nchannels'], bias_dataset[var].isel(npredictors=i).values)
+        # Create a new dataset to store the bias data
+        bias_dataset = xr.open_dataset(bias_file)
 
-        # Copy the number_obs_assimilated variable (which only has channel as dimension)
-        new_data_vars['bias::number_obs_assimilated'] = \
-            (['nchannels'], bias_dataset['number_obs_assimilated'].values)
+        # Get record dimension size
+        self.logger.assert_abort(len(bias_dataset['Record']) == 1, 'This code currently only  '
+                                 'supports reading VarBC files where the Record diminsion is 1')
 
-        # Create a new dataset with the new variables and existing 'channels' and
-        # 'number_obs_assimilated'
-        flat_dataset = xr.Dataset(
-            data_vars=new_data_vars,
-            coords={
-                'nchannels': bias_dataset['channels'],
-            },
-            attrs=bias_dataset.attrs
+        # Loop over groups, open and store in bias_dataset
+        for group in groups:
+            dsg = xr.open_dataset(bias_file, group=group)
+
+            # Rename variables with group
+            dsg = dsg.rename_vars({var: f'{group}::{var}' for var in dsg.data_vars})
+
+            # Store the data in the bias_dataset
+            bias_dataset = xr.merge([bias_dataset, dsg])
+
+        # Now add coordinate for channels dimension going from 0 to channel
+        bias_dataset = bias_dataset.assign_coords(
+            {'Channel': range(len(bias_dataset.Channel))}
         )
 
-        # Rename the dimension/coordinate from nchannels to channel
-        flat_dataset = flat_dataset.rename_dims({'nchannels': 'Channel'})
+        # Squeeze the record dimension and remove record coordinate
+        bias_dataset = bias_dataset.squeeze('Record')
+        bias_dataset = bias_dataset.drop_vars('Record')
+
+        # Rename numberObservationsUsed to Bias::numberObservationsUsed
+        bias_dataset = bias_dataset.rename_vars(
+            {'numberObservationsUsed': 'Bias::numberObservationsUsed'}
+        )
+
+        # Remove attrributes from the dataset
+        bias_dataset.attrs = {}
 
         # Read the t-lapse file (text) into a dataset
         with open(lapse_file, 'r') as f:
@@ -131,10 +121,10 @@ class JediVariationalBiasCorrection(EvaDatasetBase):
         lapse_data = np.array([float(line.split()[2]) for line in lapse_data])
 
         # Store lapse data in flat_dataset with channel as dimension/coordinate
-        flat_dataset['bias::tlapse'] = xr.DataArray(lapse_data, dims=['Channel'])
+        bias_dataset['Bias::tlapse'] = xr.DataArray(lapse_data, dims=['Channel'])
 
-        # Add flat_dataset to data_collections
-        data_collections.create_or_add_to_collection(collection_name, flat_dataset)
+        # Add bias_dataset to data_collections
+        data_collections.create_or_add_to_collection(collection_name, bias_dataset)
 
     # ----------------------------------------------------------------------------------------------
 
