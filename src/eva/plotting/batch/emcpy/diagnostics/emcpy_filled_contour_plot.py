d6939d155e59f121b2d15e376c83224dcd1d89c1
Kevin Dougherty <kevin.dougherty@noaa.gov>
2025-02-05 18:53:53 +0000

    add filled contour script

---
+++ src/eva/plotting/batch/emcpy/diagnostics/emcpy_filled_contour_plot.py
@@ -0,0 +1,50 @@
+from eva.eva_path import return_eva_path
+from eva.utilities.config import get
+from eva.utilities.utils import get_schema, update_object
+import emcpy.plots.plots
+import os
+
+from eva.plotting.batch.base.diagnostics.filled_contour_plot import FilledContourPlot
+
+# --------------------------------------------------------------------------------------------------
+
+
+class EmcpyFilledContourPlot(FilledContourPlot):
+
+    """
+    EmcpyFilledContourPlot class is a subclass of the FilledContourPlot class, designed for configuring
+    and plotting contour plot visualizations using the emcpy library.
+
+    Attributes:
+        Inherits attributes from the FilledContourPlot class.
+
+    Methods:
+        configure_plot(): Configures the plotting settings for the filled contour plot.
+    """
+
+    def configure_plot(self):
+
+        """
+        Configures the plotting settings for the filled contour plot.
+
+        Returns:
+            plotobj: The configured plot object for emcpy filled contour plots.
+        """
+
+        # Create declarative plotting ContourPlot object
+        # -------------------------------------------
+        self.plotobj = emcpy.plots.plots.FilledContourPlot(self.xdata, self.ydata, self.z)
+
+        # Get defaults from schema
+        # ------------------------
+        layer_schema = self.config.get('schema', os.path.join(return_eva_path(), 'plotting',
+                                       'batch', 'emcpy', 'defaults', 'filled_contour_plot.yaml'))
+        new_config = get_schema(layer_schema, self.config, self.logger)
+        delvars = ['x', 'y', 'z', 'type', 'schema']
+        for d in delvars:
+            new_config.pop(d, None)
+        self.plotobj = update_object(self.plotobj, new_config, self.logger)
+
+        return self.plotobj
+
+# --------------------------------------------------------------------------------------------------
